38d762223a47bafe3c53d35b32b167991beb99ba
ramazzzini <ramazan98derbent@gmail.com>
2023-10-23 18:04:44 +0500

    start 1.10 redQuery

---
+++ src/core/red-query/red-query.lib.js
@@ -0,0 +1,80 @@
+import { extractErrorMessage } from "./extract-error-message";
+
+/**
+ * RedQuery is a minimalistic library for handling API requests.
+ * Fetch data from the API with provided options.
+ *
+ * @param {Object} options - Configuration options for the API request.
+ * @param {string} options.path - The API endpoint path.
+ * @param {('GET'|'POST'|'PATCH'|'DELETE'|'PUT')} [options.method='GET'] - The HTTP method to use for the request.
+ * @param {Object} [options.body=null] - The request payload to send as JSON.
+ * @param {Object} [options.headers={}] - Additional headers to include with the request.
+ * @param {Function} [options.onSuccess=null] - Callback function to be called on successful response.
+ * @param {Function} [options.onError=null] - Callback function to be called on error response.
+ * @returns {Promise<{isLoading: boolean, error: string|null, data: any|null}>} - An object containing the loading state, error, and data from the response.
+ */
+export async function redQuery({
+    path, 
+    body = null, 
+    headers = {}, 
+    method = 'GET', 
+    onError = null, 
+    onSuccess = null
+}) {
+    let isLoading = true, 
+        error = null, 
+        data = null
+    const url = `${SERVER_URL}/api${path}`
+
+    /* ACCES_TOKEN from LS */
+    const accessToken = '';
+
+    const requestOptions = {
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+            ...headers
+        }
+    }
+
+    if(ACCESS_TOKEN){
+        requestOptions.headers.Authorization = `Bearer ${accessToken}`
+    }
+
+    if(body){
+        requestOptions.body = JSON.stringify(body)
+    }
+
+    try {
+        const response = await fetch(url, requestOptions)
+
+        if(response.ok){
+            data = await response.json()
+            if (onSuccess){
+                onSuccess(data)
+            }
+        } else{
+            const errorData = await response.json()
+            const errorMessage = extractErrorMessage(errorData)
+
+            if (onError){
+                onError(errorMessage)
+            }
+
+            /* Notification error */
+        }
+
+    } catch (errorData) {
+        const errorMessage = extractErrorMessage(errorData)
+
+        if (errorMessage){
+            onError(errorMessage)
+        }
+    } finally {
+        isLoading = false
+    }
+
+    return {isLoading, error, data}
+        
+
+}
